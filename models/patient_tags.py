dee0e97e66021714cf11677e8109b767d32445a0
omar <baiomayomar@gmail.com>
2024-05-19 08:31:12 +0300

    add patient tags

---
+++ models/patient_tags.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+
+class Patient(models.Model):
+    _name = 'hospital.patient.tags'
+    _description = 'Patient Tags Management'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+
+    active = fields.Boolean(string="Active", default=True, tracking=True)
+    name = fields.Char(string="Name", required=True, tracking=True)
